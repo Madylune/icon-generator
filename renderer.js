3e7f1074d7db167ae09254832b88c95d16d29611
Julie Nginn <nginn.julie@gmail.com>
2019-09-26 11:22:36 +0200

    Resize by range input OK

--- renderer.js
+++ renderer.js
@@ -1,3 +1,6 @@
+// const fs = require('fs')
+// const gm = require('gm').subClass({ imageMagick: true })
+
 function openFile(inputFile) {
   inputFile.click()
   inputFile.addEventListener('change', () => {
@@ -6,7 +9,21 @@ function openFile(inputFile) {
     reader.onloadend = () => { 
         const base64data = reader.result     
         document.getElementById('attachmentInput').value = base64data
-        document.getElementById('Preview').innerHTML = `<img class="Preview_img" src="${base64data}"/>`
+        document.getElementById('Preview').innerHTML = `<img class="Preview_img" id="Preview_img" src="${base64data}"/>`
     }
   })
+}
+
+const resizeInputRange = document.getElementById('formControlRange')
+document.getElementById('Range_label').innerHTML = `${resizeInputRange.value} px` // Default value
+
+resizeInputRange.addEventListener('change', () => {
+  let rangeValue = resizeInputRange.value
+  resizeImage(rangeValue)
+  document.getElementById('Range_label').innerHTML = `${rangeValue} px`
+})
+
+function resizeImage(sizes) {
+  document.getElementById('Preview').style.height = `${sizes}px`
+  document.getElementById('Preview').style.width = `${sizes}px`
 }
